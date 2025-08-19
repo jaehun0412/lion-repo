c2325fd5ae4a002976899a80c544f40e139ea64f
Kim Jaehun <hun970412@naver.com>
2025-08-19 10:15:12 +0900

    first day commit

---
+++ src/day01/Hello.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // 주석
+        System.out.println("hihihi!!!");
+    }
+
+    // 일반 주석 private String name;
+
+    // TODO - 기능 추가
+    public int add(int a, int b) {
+        return 0;
+    }
+
+    // FIXME - 수정 필요
+    public int count(int add) {
+        return 0;
+    }
+}

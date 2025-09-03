8e2f89995ea17528db1fe86d601f3cdc6941e27c
Kim Jaehun <hun970412@naver.com>
2025-09-03 17:38:16 +0900

    25_09_03 update

--- src/day11/ExceptionExam6.java
+++ src/day11/ExceptionExam6.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class ExceptionExam6 {
 
-    public static void inputScore(int score) {
+    public static void inputScore(int score) throws Yewei {
         // score는 점수 값을 나타내며, 0~100까지만 입력 받고 싶다.
         if (score < 0 || score > 100) {
-            throw new RuntimeException();
+            throw new Yewei();
         }
         System.out.println("학생의 점수: " + score);
     }
@@ -15,7 +15,11 @@ public class ExceptionExam6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("점수를 입력하세요: ");
-        inputScore(input.nextInt());
+        try {
+            inputScore(input.nextInt());
+        } catch (Yewei e) {
+            System.out.println(e.getMessage());
+        }
 
         System.out.println("main 종료");
     }
